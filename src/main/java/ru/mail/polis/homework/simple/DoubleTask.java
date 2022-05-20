b77f3bf667d853a005d0699845f7a693d5357cd4
Maksim <zakkubas@gmail.com>
2022-05-20 16:25:18 +0300

    Main, MainTask and IntegerTask are done
    DoubleTask is done

--- src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
+++ src/main/java/ru/mail/polis/homework/simple/DoubleTask.java
@@ -16,6 +16,15 @@ public class DoubleTask {
     public static String equation(int a, int b, int c) {
         double x1 = 0;
         double x2 = 0;
+        double d;
+        d = (b * b) - (4 * a * c);
+        if (d > 0) {
+            x1 = ((-1 * b) + Math.sqrt(d)) / (2 * a);
+            x2 = ((-1 * b) - Math.sqrt(d)) / (2 * a);
+        } else if (d == 0) {
+            x1 = -1 * (b / (2 * a));
+            x2 = x1;
+        }
         return x1 + ", " + x2;
     }
 
@@ -24,6 +33,8 @@ public class DoubleTask {
      * Пример: (0, 0, 3, 4) -> 5.0
      */
     public static float length(double x1, double y1, double x2, double y2) {
-        return 0;
+        float distance;
+        distance = (float) Math.sqrt(((x2 - x1) * (x2 - x1)) + ((y2 - y1) * (y2 - y1)));
+        return distance;
     }
 }
